f0350aad740c8c496c4a946721054852a0f0dfba
renan-olaria-soares <renan.olaria.soares@gmail.com>
2022-06-13 14:20:24 -0300

    Implementando um contador

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,12 @@
-console.log("Olá Mundo!");
-document.getElementsByTagName('h1');
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0;
 
-var heading1 = document.getElementsByTagName('h1') [0];
-heading1.style.color ='red';
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
